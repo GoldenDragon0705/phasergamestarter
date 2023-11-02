9cc5bb869e43fbcc937038e4efb75bf969c12465
felix95814 <felixmorales95814@gmail.com>
2023-11-02 05:37:09 +0000

    scene created

--- frontend/assets/js/init.js
+++ frontend/assets/js/init.js
@@ -1,8 +1,14 @@
 
 const enter = () => {
-  const socket = io();
+  const socket = io(
+  );
   const data = document.getElementById("input").value;
   console.log(data, socket)
 
   socket.emit(SOCKET_PROC.ENTER_ROOM, { data: data })
+
+  socket.on(SOCKET_PROC.START, (data) => {
+    createGame(data);
+    
+  })
 }
