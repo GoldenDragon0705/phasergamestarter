387188e0916f10bc842ccd283e633c68ce9a406f
GoldenDragon0705 <GoldenDragon0705@outlook.com>
2023-11-02 00:54:24 -0400

    create http and socket server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+require('dotenv').config();
+require('./src/socket/index.socket')(io);
+
+// get env vars
+const { SERVER_PORT } = process.env;
+// set public folder
+app.use(express.static('public'));
+// handle 404 error
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(SERVER_PORT, () => {
+  console.log('listening on *:' + SERVER_PORT);
+});
---
+++ backend/src/socket/index.socket.js
@@ -0,0 +1,11 @@
+const Socket = (io) => {
+  io.on("connection", () => {
+
+  });
+
+  io.on("disconnect", () => {
+    
+  })
+};
+
+module.exports = Socket;
